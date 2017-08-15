999696f37119bddce617af51b859effb0a5c16bc
Gabriel R Deveaux <grbdeveaux@gmail.com>
2017-08-15 18:03:25 -0230

    Removed git prepare keybinding

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -1,42 +1,25 @@
-# git
-	function git_prepare() {
-		if [ -n "$BUFFER" ];
-			then
-				BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
-		fi
-
-		if [ -z "$BUFFER" ];
-			then
-				BUFFER="git add -A; git commit -v && git push"
-		fi
-
-		zle accept-line
-	}
-	zle -N git_prepare
-	bindkey "^g" git_prepare
-
 # Edit and rerun
-	function edit_and_run() {
-		BUFFER="fc"
-		zle accept-line
-	}
-	zle -N edit_and_run
-	bindkey "^v" edit_and_run
+    function edit_and_run() {
+        BUFFER="fc"
+        zle accept-line
+    }
+    zle -N edit_and_run
+    bindkey "^v" edit_and_run
 
 # Enter
-	function enter_line() {
-		zle accept-line
-	}
-	zle -N enter_line
-	bindkey "^o" enter_line
+    function enter_line() {
+        zle accept-line
+    }
+    zle -N enter_line
+    bindkey "^o" enter_line
 
 # Sudo
-	function add_sudo() {
-		BUFFER="sudo "$BUFFER
-		zle end-of-line
-	}
-	zle -N add_sudo
-	bindkey "^s" add_sudo
+    function add_sudo() {
+        BUFFER="sudo "$BUFFER
+        zle end-of-line
+    }
+    zle -N add_sudo
+    bindkey "^s" add_sudo
 
 
 #####################################
@@ -44,27 +27,44 @@
 #Ctrl-h/j/k/l which are used by tmux#
 #####################################
 ## LS
-#	function ctrl_l() {
-#		BUFFER="ls"
-#		zle accept-line
-#	}
-#	zle -N ctrl_l
-#	bindkey "^l" ctrl_l
+#    function ctrl_l() {
+#        BUFFER="ls"
+#        zle accept-line
+#    }
+#    zle -N ctrl_l
+#    bindkey "^l" ctrl_l
 #
 #
 ## home
-#	function goto_home() {
-#		BUFFER="cd ~/"$BUFFER
-#		zle end-of-line
-#		zle accept-line
-#	}
-#	zle -N goto_home
-#	bindkey "^h" goto_home
+#    function goto_home() {
+#        BUFFER="cd ~/"$BUFFER
+#        zle end-of-line
+#        zle accept-line
+#    }
+#    zle -N goto_home
+#    bindkey "^h" goto_home
 #
 ## up
-#	function up_widget() {
-#		BUFFER="cd .."
-#		zle accept-line
-#	}
-#	zle -N up_widget
-#	bindkey "^k" up_widget
+#    function up_widget() {
+#        BUFFER="cd .."
+#        zle accept-line
+#    }
+#    zle -N up_widget
+#    bindkey "^k" up_widget
+# git
+# function git_prepare() {
+#     if [ -n "$BUFFER" ];
+#         then
+#             BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
+#     fi
+#
+#     if [ -z "$BUFFER" ];
+#         then
+#             BUFFER="git add -A; git commit -v && git push"
+#     fi
+#
+#     zle accept-line
+# }
+# zle -N git_prepare
+# bindkey "^g" git_prepare
+
