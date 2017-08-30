e296491e2eb1dfcece80053d23054f2f45b34e2e
Gabriel R Deveaux <grbdeveaux@gmail.com>
2017-08-30 13:48:53 -0300

    Fixed tex loading Enable tex folding ONLY for texmode, which prevented loading other files.

--- emacs/settings/textweaks.el
+++ emacs/settings/textweaks.el
@@ -1,6 +1,6 @@
 (add-hook 'TeX-mode-hook (lambda ()
                              (TeX-fold-mode 1)))
 
-(add-hook 'find-file-hook 'TeX-fold-buffer t)
+(add-hook 'TeX-mode-hook 'TeX-fold-buffer t)
 
 (provide 'textweaks)
