76c926d40cd86711f1141d1b70a962bdef451dea
Gabriel R Deveaux <grbdeveaux@gmail.com>
2017-08-30 23:10:30 -0300

    Added files relevant to helm, evil leader, and frame cmds

---
+++ emacs/settings/confhelm.el
@@ -0,0 +1,14 @@
+;; Configurations for HELM
+(require 'helm)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x r b") 'helm-bookmarks)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-s") 'helm-occur)
+
+
+(provide 'confhelm)
