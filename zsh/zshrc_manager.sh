609457223868b33ad4b02a4245b04d68193e2057
Gabriel Deveaux <grbdeveaux@gmail.com>
2018-07-24 21:43:09 -0300

    Updated zshrc files paths in zshrc_manager

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -2,8 +2,8 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # Source other zsh files
-source ~/dotfiles/zsh/zshrc.sh
-source ~/dotfiles/zsh/zshrc_alias.sh
+source ~/Documents/GitHub/dotfiles/zsh/zshrc.sh
+source ~/Documents/GitHub/dotfiles/zsh/zshrc_alias.sh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
